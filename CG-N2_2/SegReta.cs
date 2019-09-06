4d6f6f30fbd42bc70faeff2709ccf7e177d342c4
Gustavo Westarb <gustavowestarb45@gmail.com>
2019-09-06 13:00:12 -0300

    só para voltar o stash"

---
+++ CG-N2_2/SegReta.cs
@@ -0,0 +1,13 @@
+using System;
+using CG_Biblioteca;
+
+namespace gcgcg
+{
+    internal class SegReta : ObjetoAramado
+    {
+        public SegReta(string rotulo) : base(rotulo)
+        {
+            
+        }
+    }
+}
